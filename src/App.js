697f420d442ec4a04904254e9e10a10e5c7107d4
Ian Guy <iguy@udel.edu>
2025-02-11 16:03:19 -0500

    updating css of members page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import peoplecoding from './peoplecoding.gif';
 import bluehen from './bluehen.gif';
 import Navbar from './navbar';
 import './App.css';
@@ -16,8 +15,8 @@ function Home(){
       <img src={bluehen} alt="blue hen" style ={ {width : "500px"}}/>
       <h3>We are a group of professionals dedicated to advancing the field of computing.</h3>
       <h4>Join us in our mission to promote excellence in computer science.</h4>
-      <p>The Computing Professional Society at the University of Delaware aims to bring together a community of students who are enthusiastic about expanding their knowledge of computer science. Our goal is to advance professional growth, encourage collaboration, and support the sharing of knowledge in the domain of computer science. Our goal is to offer members avenues for networking, skill development, and insight into industry practices, equipping them to thrive as professionals in the constantly changing computing environment.</p>
-      <h5>Contact us for more information @999-999-9999.</h5>
+      <p>The Society For Professional Computing at the University of Delaware aims to bring together a community of students who are enthusiastic about expanding their knowledge of computer science. Our goal is to advance professional growth, encourage collaboration, and support the sharing of knowledge in the domain of computer science. Our goal is to offer members avenues for networking, skill development, and insight into industry practices, equipping them to thrive as professionals in the constantly changing computing environment.</p>
+      <h5>Contact us for more information at udelcomputingsociety@gmail.com.</h5>
     </div>
   );
 
--- src/members.js
+++ src/members.js
@@ -6,10 +6,13 @@ function Members() {
          
             <h1>Members List</h1>
             <a href="https://linkedin.com/in/david-thissen-777312307" target="_blank" rel="noopener noreferrer">President: David Thissen</a>
+            <a href="https://linkedin.com/in/ian-guy" target="_blank" rel="noopener noreferrer">Vice President: Ian Guy</a>
+            <a href="https://linkedin.com/in/nathan-graddon-650070277" target="_blank" rel="noopener noreferrer">Treasurer: Nathan Graddon</a>
+
+
       <h3>Other Members</h3>
       <a href="https://linkedin.com/in/jacob-chacko-9967a12ab" target="_blank" rel="noopener noreferrer">Jacob Chacko</a>
      
-      <a href="https://linkedin.com/in/ian-guy" target="_blank" rel="noopener noreferrer">Ian Guy</a>
       
       <a href="https://linkedin.com/in/nicholas-j-farraj-7b3341345" target="_blank" rel="noopener noreferrer">Nick Farraj</a>
           
@@ -29,7 +32,6 @@ function Members() {
             
             <a href="https://linkedin.com/in/matthew-williams-323947210" target="_blank" rel="noopener noreferrer">Matthew Williams</a>
            
-            <a href="https://linkedin.com/in/nathan-graddon-650070277" target="_blank" rel="noopener noreferrer">Treasurer: Nathan Graddon</a>
            
             <a href="https://linkedin.com/in/ryan-padrone-42a54a299" target="_blank" rel="noopener noreferrer">Ryan Padrone</a>
 
