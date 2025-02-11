cf3a8f011ebf02d943a328d48eb8335d0c97055f
Ian Guy <iguy@udel.edu>
2025-02-10 21:35:10 -0500

    added roster members with linkedin attatched

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function Home(){
       <img src={peoplecoding} alt="people coding" style ={ {width : "500px"}}/>
       <h3>We are a group of professionals dedicated to advancing the field of computing.</h3>
       <h4>Join us in our mission to promote excellence in computer science.</h4>
-      <p>The Computing Professional Society at the University of Delaware aims to bring together a community of students who are enthusiastic about expanding their knowledge of computer sciece. Our goal is to advance professional growth, encourage collaboration, and support the sharing of knowledge in the domain of computer science. Our goal is to offer members avenues for networking, skill development, and insight into industry practices, equipping them to thrive as professionals in the constantly changing computing environment.</p>
+      <p>The Computing Professional Society at the University of Delaware aims to bring together a community of students who are enthusiastic about expanding their knowledge of computer science. Our goal is to advance professional growth, encourage collaboration, and support the sharing of knowledge in the domain of computer science. Our goal is to offer members avenues for networking, skill development, and insight into industry practices, equipping them to thrive as professionals in the constantly changing computing environment.</p>
       <h5>Contact us for more information @999-999-9999.</h5>
     </div>
   );
--- src/members.js
+++ src/members.js
@@ -8,10 +8,40 @@ function Members() {
       <h3>Other Members</h3>
       <a href="https://linkedin.com/in/jacob-chacko-9967a12ab" target="_blank" rel="noopener noreferrer">Jacob Chacko</a>
       <br />
+      <br />
       <a href="https://linkedin.com/in/ian-guy" target="_blank" rel="noopener noreferrer">Ian Guy</a>
+      <br />
       <br />
       <a href="https://linkedin.com/in/nicholas-j-farraj-7b3341345" target="_blank" rel="noopener noreferrer">Nick Farraj</a>
             <br />
+            <br />
+
+            <a href="https://linkedin.com/in/bfvr" target="_blank" rel="noopener noreferrer">Brendan van Riper</a>
+            <br />
+            <br />
+
+            <a href="https://linkedin.com/in/brynnmaleryn" target="_blank" rel="noopener noreferrer">Brynn Maleryn</a>
+            <br />
+            <br />
+            <a href="https://linkedin.com/in/dylan-frajerman-14327621a" target="_blank" rel="noopener noreferrer">Dylan Frajerman</a>
+            <br />
+            <br />
+
+            <a href="https://linkedin.com/in/felixdbenz" target="_blank" rel="noopener noreferrer">Felix Benz</a>
+            <br />
+            <br />
+
+            <a href="https://linkedin.com/in/logan-ponik" target="_blank" rel="noopener noreferrer">Logan Ponik</a>
+            <br />
+            <br />
+            <a href="https://linkedin.com/in/matthew-williams-323947210" target="_blank" rel="noopener noreferrer">Matthew Williams</a>
+            <br />
+            <br />
+            <a href="https://linkedin.com/in/nathan-graddon-650070277" target="_blank" rel="noopener noreferrer">Nathan Graddon</a>
+            <br />
+            <br />
+            <a href="https://linkedin.com/in/ryan-padrone-42a54a299" target="_blank" rel="noopener noreferrer">Ryan Padrone</a>
+
         </div>
     )
 }
